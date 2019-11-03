738c5ebabc1325b597379d4cbecda15613fe71a7
kongyl <kongyl@126.com>
2019-11-03 17:28:08 +0800

    modify mysql

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,1 +1,1 @@
-docker run --name mysql -e MYSQL_ROOT_PASSWORD=login -p 3306:3306 -d mysql --collation-server=utf8mb4_unicode_ci --default-authentication-plugin=mysql_native_password
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=login -p 3306:3306 -d mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --default-authentication-plugin=mysql_native_password
