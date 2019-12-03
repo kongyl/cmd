56b7a10f4724d22decb9966b693bf3ee4d9c2c23
kongyl <kongyl@126.com>
2019-12-03 09:21:44 +0800

    docker network

---
+++ docker/network.sh
@@ -0,0 +1,1 @@
+docker network create --subnet=10.0.0.0/24 my_net
---
+++ docker/pgadmin.sh
@@ -0,0 +1,1 @@
+docker run --name pgadmin --network=my_net --ip=172.18.0.102 -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' -d dpage/pgadmin4
