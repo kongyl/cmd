20b4bb07847a250ab7183f75e300efecb8e595b3
kongyl <kongyl@126.com>
2019-12-13 16:13:44 +0800

    remote hbase

--- hadoop/hadoop.sh
+++
@@ -1,4 +0,0 @@
-docker run --name master --network ubuntu --ip 10.4.0.2 -itd kongyl/hadoop /start.sh
-docker run --name slave01 --network ubuntu --ip 10.4.0.11 -itd kongyl/hadoop /start.sh
-docker run --name slave02 --network ubuntu --ip 10.4.0.12 -itd kongyl/hadoop /start.sh
-
--- hbase/docker.sh
+++
@@ -1,4 +0,0 @@
-docker run --name hbase --network ubuntu --ip 10.4.0.4 -itd kongyl/hbase /start.sh
-docker run --name region01 --network ubuntu --ip 10.4.0.31 -itd kongyl/hbase /start.sh
-docker run --name region02 --network ubuntu --ip 10.4.0.32 -itd kongyl/hbase /start.sh
-
