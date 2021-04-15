e7b5b27b4633c510908ca738d9b80caffaf480ee
kongyl <kongyl@126.com>
2021-04-15 08:55:08 +0800

    update for docker

--- docker/postgres.sh
+++ docker/postgres.sh
@@ -1,9 +1,9 @@
 # postgres
-docker run --name postgres -e POSTGRES_PASSWORD=postgres --network=postgres --ip=10.2.0.2 -d postgres
-docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=postgres --restart=always --network=postgres --ip=10.2.0.2 -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=postgres --restart=always -p 5432:5432 -d postgres
 # postgis
 docker exec -it postgres /bin/bash
 apt install postgresql-13-postgis-3
 
 # pgadmin
-docker run --name pgadmin -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' --network=postgres --ip=10.2.0.3 -d dpage/pgadmin4
+docker run --name pgadmin -e 'PGADMIN_DEFAULT_EMAIL=kongyl@126.com' -e 'PGADMIN_DEFAULT_PASSWORD=tuxiangshi' --restart=always --network=postgres --ip=10.2.0.3 -d dpage/pgadmin4
