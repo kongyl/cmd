c7760be12a5012c8291de2948fd9f41b7f54bfc2
kongyl <kongyl@126.com>
2021-05-28 21:12:44 +0800

    move to docker

--- docker/cesium-terrain-builder.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v /home/kongyl/Test/data:/data homme/cesium-terrain-builder ctb-tile --output-dir /data/ou
-t/terrain /data/in/dem.tif
--- docker/ubuntu.sh
+++
@@ -1,4 +0,0 @@
-docker run --name ubuntu -dit ubuntu /bin/bash
-
-docker attach ubuntu
-# Ctrl+p+q to detach
