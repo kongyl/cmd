02919d034448caa339f6777e467939b43c149ce6
kongyl <kongyl@126.com>
2023-12-04 14:34:06 +0800

    gdal2tiles

--- gdal/cmake.sh
+++
@@ -1,4 +0,0 @@
-make build
-cd build
-cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_BUILD_TYPE=Release ..
-cmake --build .
--- gdal/gdal2tiles.sh
+++ gdal/gdal2tiles.sh
@@ -1,2 +1,4 @@
 # gdal2tiles
 docker run --rm -v /home/kongyl/Donwloads:/data osgeo/gdal gdal2tiles.py -s EPSG:4527 -z 0-11 /data/1310032018BJ2DOM1.tif /data/test
+
+docker run -it --rm -v /root/Test/gdal:/data ghcr.io/osgeo/gdal:ubuntu-full-3.7.3 gdal2tiles.py --zoom=0-9 --xyz --tiledriver=WEBP /data/world.tif /data/t07
